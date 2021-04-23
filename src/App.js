ef7e41f13469b3ab1e39f465632e288f813039ec
karamaBattikh <battikh.karama@gmail.com>
2021-04-23 02:07:03 +0100

    add reset css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import ResetCss from "./styles/reset-css";
+
 export default function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <ResetCss />
+      app
+    </div>
+  );
 }
---
+++ src/styles/reset-css.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+const reset = createGlobalStyle`
+  html {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+  }
+  *, *:before, *:after {
+    -webkit-box-sizing: inherit;
+    -moz-box-sizing: inherit;
+    box-sizing: inherit;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  img,
+  var,
+  i,
+  center,
+  ul,
+  li,
+  form,
+  label,
+  table,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  footer,
+  header,
+  menu,
+  nav {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+`;
+
+export default reset;
